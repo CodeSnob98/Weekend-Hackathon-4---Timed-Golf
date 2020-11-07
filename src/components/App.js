9cd6eab42fca60124b583f296bfa866fa8c6a1b4
CodeSnob98 <63289568+CodeSnob98@users.noreply.github.com>
2020-11-07 18:15:01 +0530

    change1

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,55 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = { time: 0, x: 0, y: 0, clicked: false };
+    this.handleClick = this.handleClick.bind(this);
+    this.handleArrow = this.handleArrow.bind(this);
+    this.r = React.createRef();
+  }
+  handleClick() {
+    this.setState({ clicked: true });
+    this.r.current = setInterval(() => {
+      this.setState({ time: this.state.time + 1 });
+    }, 1000);
+  }
+  handleArrow(event) {
+    if (this.state.clicked) {
+      let key = event.keyCode;
+      if (key === 39) {
+        this.setState({ x: this.state.x + 5 });
+      } else if (key === 37) {
+        this.setState({ x: this.state.x - 5 });
+      } else if (key === 38) {
+        this.setState({ y: this.state.y - 5 });
+      } else if (key === 40) {
+        this.setState({ y: this.state.y + 5 });
+      }
+    }
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.r.current);
+    }
   }
   componentDidMount() {
-    
+    document.addEventListener("keydown", (event) => this.handleArrow(event));
   }
 
-  componentWillUnmount() {
-    
-  }
-
-
+  componentWillUnmount() {}
 
   render() {
     return (
- <>
-</>
+      <>
+        <button className="start" onClick={this.handleClick}>
+          Start
+        </button>
+        <>
+          <div
+            className="ball"
+            style={{ left: this.state.x + "px", top: this.state.y + "px" }}
+          ></div>
+          <div className="hole" style={{ left: "250px", top: "250px" }}></div>
+          <div className="heading-timer">{this.state.time}</div>
+        </>
+      </>
     );
   }
 }
